42359651ce7bc470559fd63916df6023ae3ce637
Samisha <samishaboywar24@gmail.com>
2025-12-13 11:36:17 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Samisha Boywar",
+    author_email="samishaboywar24@gmail.com",
+    packages=find_packages()
+)
